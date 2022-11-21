8bcb50378fb688d9629954fb7eb3f9f51f6995ce
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2022-11-20 23:05:50 -0300

    atualizaçao

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -1,63 +1,67 @@
 const mario = document.querySelector('#mario')
 const pedra = document.querySelector('.pedra')
-const pontuaçao = document.querySelector('#ponto')
+const score = document.querySelector('#ponto')
 const botao = document.querySelector('#botao')
+let largura = window.screen.width
+let pedra_position = pedra.offsetLeft
+let botoom = getComputedStyle(mario).bottom
+/**________________________//____________________ */
+if(largura > 896){
 
-const s = 0
-const pontos =()=>{
+    
+    function fim(){
+                pedra.style.animation ='none'
+                pedra.style.left = '150px'
+                mario.classList.add('morte')
+    }
+    const pulo = (e)=>{ 
+        /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
+        if((e.key=='ArrowUp')||(e.key == 'w')){
+            mario.classList.add('jump')
+            setTimeout(()=>{
+                mario.classList.remove('jump')
+            },500 )
+        }
+    }
+    const loop = setInterval(()=>{ 
+        if((pedra.getBoundingClientRect().left <= 160 )&&  (pedra.getBoundingClientRect().left >= 145) && (mario.getBoundingClientRect().bottom >= 450.81752014160156 )){
+                fim()
+        }  
+    },0)
+    
+
+addEventListener('keydown',(e)=>pulo(e))
 
 
-    s+=10
-
-    return s
-}
-
-const pulo = (e)=>{ /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
-    if((e.key=='ArrowUp')||(e.key == 'w')){
+}else if(largura<=896){
+    const pulo = (e)=>{ 
+    /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
+    
         mario.classList.add('jump')
-
-        clearInterval(teste)
+       
         setTimeout(()=>{
             mario.classList.remove('jump')
         },500 )
-    } 
-    
-}
-const pulo1 = ()=>{ /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
-        
-        mario.classList.add('jump')
-        setTimeout(()=>{
-            mario.classList.remove('jump')
-        },400 )
-    
-    
-}
-
- /**<= 305.0575256347656
-  * 
-  * > 297.2382507324219
-      && (mario.getBoundingClientRect().bottom > 450.81752014160156)
- */
-const loop = setInterval(()=>{
-   
-    
-    if((pedra.getBoundingClientRect().left <= 301.0575256347656 )&&  (pedra.getBoundingClientRect().left >= 297.2382507324219) && (mario.getBoundingClientRect().bottom > 450.81752014160156 )){
-
-      pedra.style.animation ='none'
-        pedra.style.left = '150px'
+    }
+    function fim(){
+        pedra.style.animation ='none'
+        pedra.style.left = '120px'
         mario.classList.add('morte')
     }
-    
+    const loop = setInterval(()=>{ 
+        let botoom = window.getComputedStyle(mario).bottom
+            console.log(botoom )
+         if((pedra.getBoundingClientRect().left <= 75 ) &&  (pedra.getBoundingClientRect().x > 70) && (botoom <= 10+'px')){
+            
+            
+                fim()
+        }  
+        
+    },0)
 
-    
-},1)
+   
 
-const teste = setInterval(()=>{
+    botao.addEventListener('click',pulo)
     
-    
-})
-
-
-    
-addEventListener('keydown',(e)=>pulo(e))
-botao.addEventListener('click',pulo1)
+}
+ 
