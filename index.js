ccf3423a23466b57e94366a883764828bc07cdbf
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2023-09-25 23:28:26 -0300

    atualizacao

--- index.js
+++ index.js
@@ -9,53 +9,65 @@ let largura = window.screen.width
 let pedra_position = pedra.offsetLeft
 let botoom = getComputedStyle(mario).bottom
 
-audiofundo.setAttribute('src','./audio/fundo.mp3')
-audiofundo.play()
 
 
-/**________________________//____________________ */
-if(largura > 896){
+/**__________________
+ * ______//____________________ */
+if(largura <= 896){
 
      
      
     function fim(){   
 
         game.appendChild(audio)
-        audio.setAttribute('src','./audio/derruba.mp3')
+        audio.setAttribute('src','./audio/wndzp.mp3')
         pedra.style.animation ='none'
         pedra.style.left = '150px'
-        mario.classList.add('morte')       
+        mario.classList.add('morte')  
+        game.classList.add('sefudeu')     
         audio.play()
         audiofundo.pause()
         clearInterval(loop)
      }           
     // }
-    const pulo = (e)=>{ 
+    const pulo = ()=>{ 
         /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
         
 
-        if((e.key=='ArrowUp')||(e.key == 'w')){
+        
             mario.classList.add('jump')
-            audio.setAttribute('src','./audio/ladrao.mp3') 
+            audio.setAttribute('src','./audio/eu-gostumm.mp3') 
             audio.play()
             setTimeout(()=>{
                 mario.classList.remove('jump')
             },500 )
-        }
+        
     }
     const loop = setInterval(()=>{ 
-        if((pedra.getBoundingClientRect().left <= 160 )&&  (pedra.getBoundingClientRect().left >= 145) && (mario.getBoundingClientRect().bottom >= 450.81752014160156 )){
+        if((pedra.getBoundingClientRect().left <= 160 )&&  (pedra.getBoundingClientRect().left >= 129.04198455810547) && (mario.getBoundingClientRect().bottom > 200.81752014160156 )){
             
             fim()
 
-        }  
-    },0)
+        }
+          console.log(mario.getBoundingClientRect().bottom)
+
+    },10)
     
+    // const loop = setInterval(()=>{ 
+    //             let botoom = window.getComputedStyle(mario).bottom
+    //              if((pedra.getBoundingClientRect().left <= 78 ) &&  (pedra.getBoundingClientRect().x > 70) && (botoom <= 10+'px')){
+                    
+                    
+    //                     fim()
+    //             }  
+                
+    //         },10)
+    
+botao.addEventListener('click',pulo)
 
-addEventListener('keydown',(e)=>pulo(e))
 
-
-}else if(largura<=1096){
+}
+else if(largura ==1096){
     const pulo = ()=>{ 
     /**funçao pulo, inclui e exclui o obejto na função classse ( jump )responsavel por mover o eixo y a 180px acima  */
         audio.setAttribute('src','./audio/ladrao.mp3')
